62d3ecd48744a59cad25c0378954983c3884da3e
alinalitvinka <alinalitvinko7@gmail.com>
2020-10-19 17:55:46 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = [];
+    let code = '';
+  for (i = 0; i < expr.length; i = i + 10) {
+  if (expr.slice(i) === '*') {
+    result.push(" ");
+  } else for (j = 0; j < 9; j = j + 2) {
+    if (expr.slice(i, i+10).slice(j, j+2) === '10') {
+      code += '.';
+    } else if (expr.slice(i, i+10).slice(j, j+2) === '11') {
+      code += '-';
+    }
+  } result.push(MORSE_TABLE[code]);
+    code = '';
+} return result.join('');
 }
 
 module.exports = {
